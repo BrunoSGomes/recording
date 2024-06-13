c508819c741793f74ab805138af7d9c2e2775efb
Bruno Gomes <brunogomes.dev@yahoo.com>
2024-06-13 20:51:29 -0300

    chore(content): map entity and check movie type

--- src/persistence/repository/content.repository.ts
+++ src/persistence/repository/content.repository.ts
@@ -1,7 +1,19 @@
 import { Injectable } from '@nestjs/common'
 import { Prisma } from '@prisma/client'
 import { ContentEntity } from '@src/core/entity/content.entity'
+import { MovieEntity } from '@src/core/entity/movie.entity'
+import { ThumbnailEntity } from '@src/core/entity/thumbnail.entity'
+import { VideoEntity } from '@src/core/entity/video.entity'
 import { PrismaService } from '@src/persistence/prisma/prisma.service'
+
+const contentInclude = Prisma.validator<Prisma.ContentInclude>()({
+  Movie: {
+    include: {
+      Video: true,
+      Thumbnail: true
+    }
+  }
+})
 
 @Injectable()
 export class ContentRepository {
@@ -45,6 +57,83 @@ export class ContentRepository {
     } catch (error) {
       this.handleAndThrowError(error)
     }
+  }
+
+  async findById(id: string): Promise<ContentEntity | undefined> {
+    try {
+      const content = await this.model.findUnique({
+        where: { id },
+        include: {
+          Movie: {
+            include: { Video: true, Thumbnail: true }
+          }
+        }
+      })
+
+      if (!content) return
+
+      return this.mapToEntity(content)
+    } catch (error) {
+      this.handleAndThrowError(error)
+    }
+  }
+
+  private mapToEntity<
+    T extends Prisma.ContentGetPayload<{ include: typeof contentInclude }>
+  >(content: T): ContentEntity {
+    if (!content.Movie) {
+      throw new Error('Movie must be defined')
+    }
+
+    const contentEntity = ContentEntity.createFrom({
+      id: content.id,
+      type: content.type,
+      title: content.title,
+      description: content.description,
+      createdAt: new Date(content.createdAt),
+      updatedAt: new Date(content.updatedAt)
+    })
+    if (this.isMovie(content) && content.Movie.Video) {
+      contentEntity.addMedia(
+        MovieEntity.createFrom({
+          id: content.Movie.id,
+          createdAt: new Date(content.Movie.createdAt),
+          updatedAt: new Date(content.Movie.updatedAt),
+          video: VideoEntity.createFrom({
+            id: content.Movie.Video.id,
+            url: content.Movie.Video.url,
+            duration: content.Movie.Video.duration,
+            sizeInKb: content.Movie.Video.sizeInKb,
+            createdAt: new Date(content.Movie.Video.createdAt),
+            updatedAt: new Date(content.Movie.Video.updatedAt)
+          })
+        })
+      )
+      if (content.Movie.Thumbnail) {
+        contentEntity.getMedia()?.addThumbnail(
+          ThumbnailEntity.createFrom({
+            id: content.Movie.Thumbnail.id,
+            url: content.Movie.Thumbnail.url,
+            createdAt: new Date(content.Movie.Thumbnail.createdAt),
+            updatedAt: new Date(content.Movie.Thumbnail.updatedAt)
+          })
+        )
+      }
+    }
+    return contentEntity
+  }
+
+  private isMovie(content: unknown): content is Prisma.ContentGetPayload<{
+    include: {
+      Movie: {
+        include: { Video: true }
+      }
+    }
+  }> {
+    if (typeof content === 'object' && content !== null && 'Movie' in content) {
+      return true
+    }
+    return false
   }
 
   private extractErrorMessage(error: unknown): string {
